50c6caf46553c5404f35eb9a961b93937452a5d6
lihailin <1234567890@qq.com>
2022-11-11 10:54:27 +0800

    进行了路由的临时合并

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import approvals from '@/views/approvals'
+import attendances from '@/views/attendances'
+import departments from '@/views/departments'
+import employees from '@/views/employees'
+import permission from '@/views/permission'
+import salaries from '@/views/salaries'
+import setting from '@/views/setting'
+import social from '@/views/social'
 
 Vue.use(Router)
 
-/* Layout */
 import Layout from '@/layout'
 
 export const constantRoutes = [
@@ -26,7 +33,7 @@ export const constantRoutes = [
     children: [
       {
         path: 'dashboard',
-        name: 'Dashboard',
+        name: 'dashboard',
         component: () => import('@/views/dashboard/index'),
         meta: { title: '首页', icon: 'dashboard' }
       }
@@ -37,16 +44,16 @@ export const constantRoutes = [
   { path: '*', redirect: '/404', hidden: true }
 ]
 
+export const asyncRoutes = [approvals, attendances, departments, employees, permission, salaries, setting, social]
+
 const createRouter = () =>
   new Router({
-    // mode: 'history', // require service support
-    scrollBehavior: () => ({ y: 0 }),
-    routes: constantRoutes
+    scrollBehavior: () => ({ y: 0 }), // 管理滚动行为 如果出现滚动 切换就让 让页面回到顶部
+    routes: [...constantRoutes, ...asyncRoutes] // 临时合并所有的路由
   })
 
 const router = createRouter()
 
-// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
   const newRouter = createRouter()
   router.matcher = newRouter.matcher // reset router
