f03eefbbc1bb325d49f4fb535e30d6d51f65b2f3
lihailin <1234567890@qq.com>
2022-11-14 12:06:33 +0800

    完成了treeTools组件的单独封装

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -4,51 +4,10 @@
       <!-- 组织架构内容 头部 -->
       <el-card class="tree-card">
         <!-- 放置结构内容 -->
-        <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
-          <el-col>
-            <!-- 左侧内容 -->
-            <i class="el-icon" />
-            <span>武汉工程科技教育股份有限公司</span>
-          </el-col>
-          <el-col :span="4">
-            <!-- 右侧内容 -->
-            <el-row type="flex" justify="end" align="middle">
-              <el-col>负责人</el-col>
-              <el-col>
-                <!-- 放置下拉菜单 -->
-                <el-dropdown>
-                  <span> 操作<i class="el-icon-arrow-down" /> </span>
-                  <!-- 下拉菜单 -->
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
+        <tree-tools :tree-node="company" />
         <!-- 放置el-tree -->
         <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
-          <el-row slot-scope="{ data }" type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
-            <el-col>
-              <span>{{ data.name }}</span>
-            </el-col>
-            <el-col span="4">
-              <el-row type="flex" justify="end" align="middle">
-                <el-col><span>{{ data.manager }}</span></el-col>
-                <el-col>
-                  <el-dropdown>
-                    <span>操作<i class="el-icon-arrow-down" /></span>
-                    <el-dropdown-menu slot="dropdown">
-                      <el-dropdown-item>添加子部门</el-dropdown-item>
-                      <el-dropdown-item>编辑部门</el-dropdown-item>
-                      <el-dropdown-item>删除部门</el-dropdown-item>
-                    </el-dropdown-menu>
-                  </el-dropdown>
-                </el-col>
-              </el-row>
-            </el-col>
-          </el-row>
+          <tree-tools slot-scope="{ data }" :tree-node="data" />
         </el-tree>
       </el-card>
     </div>
@@ -56,9 +15,12 @@
 </template>
 
 <script>
+import TreeTools from './component/tree-tools.vue'
 export default {
+  components: { TreeTools },
   data() {
     return {
+      company: { name: '武汉工程科技教育股份有限公司', manager: '负责人' },
       departs: [
         { name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
         { name: '行政部', manager: '刘备' },
