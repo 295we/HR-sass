591516ed90eefb5f5f4a5a6031c6963cff47705c
lihailin <1234567890@qq.com>
2022-11-14 11:46:38 +0800

    完成el-tree组件在组织架构中的使用

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -10,14 +10,15 @@
             <i class="el-icon" />
             <span>武汉工程科技教育股份有限公司</span>
           </el-col>
-          <el-col>
+          <el-col :span="4">
             <!-- 右侧内容 -->
             <el-row type="flex" justify="end" align="middle">
               <el-col>负责人</el-col>
-              <el-col :span="4">
+              <el-col>
                 <!-- 放置下拉菜单 -->
                 <el-dropdown>
-                  <el-button type="primary"> <span>操作</span><i class="el-icon-arrow-down el-icon--right" /> </el-button>
+                  <span> 操作<i class="el-icon-arrow-down" /> </span>
+                  <!-- 下拉菜单 -->
                   <el-dropdown-menu slot="dropdown">
                     <el-dropdown-item>添加子部门</el-dropdown-item>
                   </el-dropdown-menu>
@@ -26,13 +27,49 @@
             </el-row>
           </el-col>
         </el-row>
+        <!-- 放置el-tree -->
+        <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
+          <el-row slot-scope="{ data }" type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
+            <el-col>
+              <span>{{ data.name }}</span>
+            </el-col>
+            <el-col span="4">
+              <el-row type="flex" justify="end" align="middle">
+                <el-col><span>{{ data.manager }}</span></el-col>
+                <el-col>
+                  <el-dropdown>
+                    <span>操作<i class="el-icon-arrow-down" /></span>
+                    <el-dropdown-menu slot="dropdown">
+                      <el-dropdown-item>添加子部门</el-dropdown-item>
+                      <el-dropdown-item>编辑部门</el-dropdown-item>
+                      <el-dropdown-item>删除部门</el-dropdown-item>
+                    </el-dropdown-menu>
+                  </el-dropdown>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </el-tree>
       </el-card>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      departs: [
+        { name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
+        { name: '行政部', manager: '刘备' },
+        { name: '人事部', manager: '孙权' }
+      ],
+      defaultProps: {
+        label: 'name' // 表示 从这个属性显示内容
+      }
+    }
+  }
+}
 </script>
 
 <style scoped>
